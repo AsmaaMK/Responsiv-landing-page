570309a9fe81cce1c001c7d37171da9cd88d2ca7
Asmaa M. Khalaf <asmaa.m.khalaf.fci@gmail.com>
2022-01-25 10:17:21 +0200

    Fix cards layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,38 @@
 // hamburger toggling list
 
-const list = document.querySelector('.invisible');
-const toggle_btn = document.querySelector('.burger-icon');
-const sections = document.getElementsByClassName('high-opacity');
+const list = document.querySelector(".invisible");
+const toggle_btn = document.querySelector(".burger-icon");
+const sections = document.getElementsByClassName("high-opacity");
 
-toggle_btn.addEventListener('click', () => {
-    list.classList.toggle('visible');
-})
-
+toggle_btn.addEventListener("click", () => {
+  list.classList.toggle("visible");
+});
 
 // dynamic tabs
 
 for (let i = 1; i <= 4; i++) {
-    const btn = document.getElementById('tab' + i);
-    
-    btn.addEventListener('click', () => {
-        // deactivate all tabs 
-        for (let j = 1; j <= 4; j++) {
-            const tab = document.getElementById('tab-' + j);
-            tab.style.zIndex = -1;
+  const btn = document.getElementById("tab" + i);
 
-            const btn1 = document.getElementById('tab' + j);
-            btn1.className = btn1.className.replace('active-tab', '');
-        }
+  btn.addEventListener(
+    "click",
+    () => {
+      // deactivate all tabs
+      for (let j = 1; j <= 4; j++) {
+        const tab = document.getElementById("tab-" + j);
+        tab.style.zIndex = -1;
 
-        // activate the clicked tab
-        const activTab = document.getElementById('tab-' + i);
-        activTab.style.zIndex = 1;
+        const btn1 = document.getElementById("tab" + j);
+        btn1.className = btn1.className.replace("active-tab", "");
+      }
 
-        let classN = btn.className;
-        classN += " active-tab";
-        btn.className = classN;
-    }, false);
+      // activate the clicked tab
+      const activTab = document.getElementById("tab-" + i);
+      activTab.style.zIndex = 1;
+
+      let classN = btn.className;
+      classN += " active-tab";
+      btn.className = classN;
+    },
+    false
+  );
 }
